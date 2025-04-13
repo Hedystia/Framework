6e765fc0cf6b986df41d0dfc404c343fd343689f
Zastinian <zastinian@gmail.com>
2025-04-13 09:56:44 -0600

    test: Added path ending in get

--- Tests/get.test.ts
+++ Tests/get.test.ts
@@ -3,63 +3,59 @@ import { z } from "zod";
 
 import { describe, expect, it } from "bun:test";
 
+const app = new Framework()
+  .get("/users/get", (context) => {
+    return Response.json({
+      status: "ok",
+    });
+  })
+  .get(
+    "/slug/:name",
+    (context) => {
+      context.params.name;
+      return Response.json(context.params);
+    },
+    {
+      params: z.object({
+        name: z.string(),
+      }),
+    },
+  )
+  .get(
+    "/test/test/new/random/:name/:id",
+    (context) => {
+      context.params.id;
+      return Response.json(context.params);
+    },
+    {
+      params: z.object({
+        id: z.coerce.number(),
+        name: z.string(),
+      }),
+    },
+  );
+
+app.listen(3000);
+
+type Routes = typeof app extends Framework<infer R> ? ExtractRoutes<R> : never;
+
+const client = createClient<Routes>("http://localhost:3000", app);
+
 describe("Test get route", () => {
-  const app = new Framework()
-    .get(
-      "/users/:id",
-      (context) => {
-        context.params.id;
-        return Response.json(context.params);
-      },
-      {
-        params: z.object({
-          id: z.number(),
-        }),
-      },
-    )
-    .get(
-      "/slug/:name",
-      (context) => {
-        context.params.name;
-        return Response.json(context.params);
-      },
-      {
-        params: z.object({
-          name: z.string(),
-        }),
-      },
-    )
-    .get(
-      "/test/test/new/random/:name/:id",
-      (context) => {
-        context.params.id;
-        return Response.json(context.params);
-      },
-      {
-        params: z.object({
-          id: z.coerce.number(),
-          name: z.string(),
-        }),
-      },
-    );
-
-  app.listen(3000);
-
-  type Routes = typeof app extends Framework<infer R> ? ExtractRoutes<R> : never;
-
   it("should return a response", async () => {
-    const client = createClient<Routes>("http://localhost:3000", app);
-
     const slug = await client.slug.name("sally").get();
 
     expect(slug).toEqual({ name: "sally" });
   });
 
   it("should return a response with params", async () => {
-    const client = createClient<Routes>("http://localhost:3000", app);
-
     const test = await client.test.test.new.random.name("sally").id(123).get();
 
     expect(test).toEqual({ id: 123, name: "sally" });
+  });
+
+  it("should return a response for path ending in 'get'", async () => {
+    const response = await client.users.get.get();
+    expect(response).toEqual({ status: "ok" });
   });
 });
