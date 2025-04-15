79a8a61697b353ec38c8d568c35f85d3b089c52c
Zastinian <zastinian@gmail.com>
2025-04-15 14:44:24 -0600

    feat: Added tsup config

---
+++ Package/tsup.config.ts
@@ -0,0 +1,19 @@
+import { defineConfig } from "tsup";
+
+export default defineConfig(({ watch = false }) => ({
+  clean: true,
+  dts: true,
+  entry: {
+    index: "src/index.ts",
+  },
+  format: "cjs",
+  splitting: false,
+  watch,
+  esbuildOptions(options) {
+    options.minifyIdentifiers = true;
+    options.minifySyntax = true;
+    options.minifyWhitespace = true;
+    options.keepNames = false;
+  },
+  external: ["zod", "bun"],
+}));
