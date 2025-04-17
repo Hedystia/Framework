f9183afd51d4576327eb592819f7ce8a7ccc6ce3
Zastinian <zastinian@gmail.com>
2025-04-17 10:18:42 -0600

    chore: Added use example

---
+++ Examples/use.ts
@@ -0,0 +1,27 @@
+import Hedystia, { z } from "hedystia";
+import { createClient } from "@hedystia/client";
+
+const app = new Hedystia().get(
+  "/",
+  () => {
+    return Response.json({
+      message: "Hello, world!",
+    });
+  },
+  {
+    response: z.object({
+      message: z.string(),
+    }),
+  },
+);
+
+const app_1 = new Hedystia().use("/api", app).listen(3000);
+
+const client = createClient<typeof app_1>("http://localhost:3000");
+
+const { error, data } = await client.api.index.get();
+
+console.log(`Error: ${error}`);
+console.log(`Data: ${data?.message}`);
+
+app_1.close();
