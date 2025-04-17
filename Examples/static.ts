0c1d47ce0b6af2f18dcdce2c15f70faa014b4b44
Zastinian <zastinian@gmail.com>
2025-04-17 12:08:26 -0600

    chore: Added static example

---
+++ Examples/static.ts
@@ -0,0 +1,23 @@
+import Hedystia from "hedystia";
+import { createClient } from "@hedystia/client";
+import html from "./static.html";
+
+const app = new Hedystia()
+  .static(
+    "/",
+    new Response(await Bun.file(html.index).text(), {
+      headers: { "Content-Type": "text/html" },
+    }),
+  )
+  .listen(3000);
+
+const client = createClient<typeof app>("http://localhost:3000");
+
+const { error, data } = await client.index.get(undefined, {
+  responseFormat: "text",
+});
+
+console.log(`Error: ${error}`);
+console.log(`Data: ${data}`);
+
+app.close();
