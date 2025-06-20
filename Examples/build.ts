d5277e98cc49ebbc29d4097192e6f478db3afda9
Zastinian <zastinian@gmail.com>
2025-06-20 17:33:45 -0600

    chore: Added build example

---
+++ Examples/build.ts
@@ -0,0 +1,34 @@
+import path from "path";
+import { createClient } from "@hedystia/client";
+import Hedystia, { h } from "hedystia";
+import type { AppRoutes } from "./server";
+
+const app = new Hedystia().get(
+  "/:id",
+  (context) => {
+    return Response.json({
+      params: context.params,
+    });
+  },
+  {
+    params: h.object({
+      id: h.number().coerce(),
+    }),
+    response: h.object({
+      params: h.object({ id: h.number() }),
+    }),
+  },
+);
+
+await app.buildTypes(path.join(__dirname, "server.d.ts")).then(() => {
+  app.listen(3000);
+});
+
+const client = createClient<AppRoutes>("http://localhost:3000");
+
+const { error, data } = await client.id(123).get();
+
+console.log(`Error: ${error}`);
+console.log(`Data: ${data?.params?.id}`);
+
+app.close();
