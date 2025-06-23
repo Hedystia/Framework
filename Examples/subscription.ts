3482894ea74ea370313755491f488e754082ef2a
Zastinian <zastinian@gmail.com>
2025-06-23 13:14:32 -0600

    chore: Added subscription example

---
+++ Examples/subscription.ts
@@ -0,0 +1,50 @@
+import { createClient } from "@hedystia/client";
+import Hedystia, { h } from "hedystia";
+
+export const app = new Hedystia()
+  .subscription(
+    "/guild/:id",
+    async ({ params }) => {
+      const defaultLang = "en";
+      return {
+        id: params.id,
+        lang: defaultLang,
+      };
+    },
+    {
+      params: h.object({
+        id: h.string(),
+      }),
+    },
+  )
+  .post(
+    "/guild/:id",
+    ({ body, params }) => {
+      app.publish(`/guild/${params.id}`, { id: params.id, lang: body.lang });
+      return "ok";
+    },
+    {
+      params: h.object({
+        id: h.string(),
+      }),
+      body: h.object({
+        lang: h.options(h.literal("en"), h.literal("es")),
+      }),
+    },
+  )
+  .listen(3000);
+
+const client = createClient<typeof app>("http://localhost:3000");
+
+client.guild.id("902265905638150164").subscribe(({ data }) => {
+  console.log(data);
+});
+
+setTimeout(async () => {
+  await client.guild.id("902265905638150164").post({
+    lang: "es",
+  });
+  await client.guild.id("111111111111111111").post({
+    lang: "es",
+  });
+}, 3000);
