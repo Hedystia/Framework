fb81efc6e96a23e9c7f8cbac3c5679008c02e70b
Zastinian <zastinian@gmail.com>
2025-11-19 16:41:58 -0600

    test: Return old expected content

--- Tests/build.test.ts
+++ Tests/build.test.ts
@@ -44,7 +44,7 @@ describe("Build Process", () => {
 
     const generatedContent = await file(typesFilePath).text();
     const expectedContent =
-      '// Automatic Hedystia type generation\nexport type AppRoutes=[{method:"GET";path:"/";params:any;query:any;body:any;headers:any;response:string;data:any;error:any},{method:"GET";path:"/users/get";params:any;query:any;body:any;headers:any;response:{ status: \'ok\' };data:any;error:any},{method:"GET";path:"/slug/:name";params:{ name: string };query:any;body:any;headers:any;response:{ name: string };data:any;error:any},{method:"GET";path:"/test/test/new/random/:name/:id";params:{ id: number; name: string };query:any;body:any;headers:any;response:{ id: number; name: string };data:any;error:any},{method:"GET";path:"/headers";params:any;query:any;body:any;headers:{ "x-test-header": string };response:{ "x-test-header": string };data:any;error:any}];';
+      '// Automatic Hedystia type generation\nexport type AppRoutes=[{method:"GET";path:"/";params:any;query:any;body:any;headers:any;response:string;data:any;error:any},{method:"GET";path:"/users/get";params:any;query:any;body:any;headers:any;response:{status:\'ok\'};data:any;error:any},{method:"GET";path:"/slug/:name";params:{name:string};query:any;body:any;headers:any;response:{name:string};data:any;error:any},{method:"GET";path:"/test/test/new/random/:name/:id";params:{id:number;name:string};query:any;body:any;headers:any;response:{id:number;name:string};data:any;error:any},{method:"GET";path:"/headers";params:any;query:any;body:any;headers:{"x-test-header":string};response:{"x-test-header":string};data:any;error:any}];';
     expect(generatedContent).toBe(expectedContent);
   });
 });
