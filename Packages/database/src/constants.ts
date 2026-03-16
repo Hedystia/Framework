1acc4a7f57be13b77357dac400d57666eaa45f18
Zastinian <zastinian@gmail.com>
2026-03-15 21:44:57 -0600

    feat: Added constants

---
+++ Packages/database/src/constants.ts
@@ -0,0 +1,9 @@
+export const MIGRATIONS_TABLE = "__hedystia_migrations";
+
+export const DEFAULT_CACHE_TTL = 60_000;
+export const DEFAULT_CACHE_MAX_TTL = 300_000;
+export const DEFAULT_CACHE_MAX_ENTRIES = 10_000;
+
+export const DEFAULT_SCHEMA_PATH = "src/database";
+export const DEFAULT_MIGRATIONS_PATH = "src/database/migrations";
+export const DEFAULT_SCHEMAS_PATH = "src/database/schemas";
