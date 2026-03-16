452a8cd761faef5bda70e8ba33fdf24c19996238
Zastinian <zastinian@gmail.com>
2026-03-15 21:50:30 -0600

    feat: Added global database export

---
+++ Packages/database/src/index.ts
@@ -0,0 +1,86 @@
+import { CacheManager } from "./cache";
+import { database } from "./core/database";
+import { TableRepository } from "./core/repository";
+import { createDriver } from "./drivers";
+import {
+  compileColumnDef,
+  compileCreateTable,
+  compileDelete,
+  compileInsert,
+  compileSelect,
+  compileUpdate,
+  compileWhere,
+} from "./drivers/sql-compiler";
+import { migration } from "./migrations/definition";
+import { generateMigrationTemplate, generateSchemaTemplate } from "./migrations/templates";
+import { ColumnBuilder } from "./schema/column";
+import * as columns from "./schema/columns";
+import { d } from "./schema/columns";
+import { SchemaRegistry } from "./schema/registry";
+import { table } from "./schema/table";
+import { Synchronizer } from "./sync/synchronizer";
+
+export {
+  CacheError,
+  DatabaseError,
+  DriverError,
+  MigrationError,
+  QueryError,
+  SchemaError,
+  SyncError,
+} from "./errors";
+
+export type {
+  CacheConfig,
+  ColumnDataType,
+  ColumnMetadata,
+  ConnectionConfig,
+  DatabaseConfig,
+  DatabaseDriver,
+  DatabaseType,
+  DeferredRefMeta,
+  DeleteOptions,
+  FileConnectionConfig,
+  InferInsert,
+  InferRow,
+  InferUpdate,
+  MigrationContext,
+  MigrationDefinition,
+  MySQLConnectionConfig,
+  QueryOptions,
+  ReferenceAction,
+  RelationQueryMap,
+  RelationsFor,
+  Repository,
+  ResolveResult,
+  SQLiteConnectionConfig,
+  TableDefinition,
+  TableMetadata,
+  UpdateOptions,
+  WhereClause,
+  WhereCondition,
+} from "./types";
+export {
+  CacheManager,
+  ColumnBuilder,
+  columns,
+  compileColumnDef,
+  compileCreateTable,
+  compileDelete,
+  compileInsert,
+  compileSelect,
+  compileUpdate,
+  compileWhere,
+  createDriver,
+  d,
+  database,
+  generateMigrationTemplate,
+  generateSchemaTemplate,
+  migration,
+  SchemaRegistry,
+  Synchronizer,
+  TableRepository,
+  table,
+};
+
+export default database;
