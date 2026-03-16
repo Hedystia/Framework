7a2d19c2cb75b34288a978da01b4fe33e34f140b
Zastinian <zastinian@gmail.com>
2026-03-16 08:31:48 -0600

    feat: Add new utility exports for debugging and reconnection

--- Packages/client/src/utils/index.ts
+++ Packages/client/src/utils/index.ts
@@ -1,2 +1,5 @@
+export type { DebugLevel } from "./debug";
+export { createLogger } from "./debug";
+export { calculateReconnectDelay, replaceTimeout } from "./reconnect";
 export { parseFormData, processResponse } from "./response";
 export { generateUUID } from "./uuid";
